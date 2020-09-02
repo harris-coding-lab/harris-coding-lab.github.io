9876771105cf92a249d096fb65c5e6241a64dfdc
Ari Anisfeld <ari.anisfeld@gmail.com>
2020-09-02 11:16:35 -0500

    hotfix

--- code/01a_reading-data.R
+++ code/01a_reading-data.R
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 library(tidyverse)
 library(readxl)
 library(haven)
@@ -27,33 +27,3 @@ head(texas_housing_data)
 nrow(texas_housing_data)
 
 names(texas_housing_data)
-=======
-library(tidyverse)
-library(readxl)
-library(haven)
-texas_housing_data <- txhousing
-
-texas_housing_data <- read_csv("texas_housing_data.csv")
-
-fed_data <- read_xlsx("SCE-Public-LM-Quarterly-Microdata.xlsx")
-
-drug_war_data <- read_dta(
-  "../data/Dataset_HighProfileCriminalViolence.dta")
-head(drug_war_data)
-
-fed_data <-read_xlsx(
-  "../data/SCE-Public-LM-Quarterly-Microdata.xlsx")
-head(fed_data)
-
-fed_data <- read_xlsx(
-    "../data/SCE-Public-LM-Quarterly-Microdata.xlsx", 
-    sheet = "Data 2013", 
-    skip = 1)
-head(fed_data)
-
-head(texas_housing_data)
-
-nrow(texas_housing_data)
-
-names(texas_housing_data)
->>>>>>> Stashed changes
